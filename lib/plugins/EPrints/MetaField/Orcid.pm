f2e957f3e436611f48d60ef5d6af1e75ce1941a3
Will Fyson <williamfyson89@hotmail.com>
2016-10-17 15:03:50 +0100

    Add ORCID validation.

--- lib/plugins/EPrints/MetaField/Orcid.pm
+++ lib/plugins/EPrints/MetaField/Orcid.pm
@@ -2,7 +2,7 @@ package EPrints::MetaField::Orcid;
 
 use strict;
 use warnings;
-
+use Data::Dumper;
 use EPrints::MetaField::Text;
 
 BEGIN {
@@ -30,11 +30,35 @@ sub validate
 {
         my( $self, $session, $value, $object ) = @_;
 
-        my @problems = $session->get_repository->call(
-                "validate_field",
-                $self,
-                $value,
-                $session );
+	my @problems;
+
+	foreach my $orcid ( @{$value} )
+	{
+
+		#get orcid digits
+		$orcid =~ s/-//g;
+		my @digits = split //, $orcid;
+		if( scalar @digits == 16 ) #check there are 16 digits...
+		{
+			#generate check digit
+			my $total = 0;
+			for my $i (0 .. (scalar @digits - 2))
+			{
+				$total = ($total + $digits[$i]) * 2;
+			}
+			my $rem = $total % 11;
+			my $res = (12 - $rem) % 11;
+			$res = $res == 10 ? "X" : $res;
+			if( $res != $digits[15] )
+			{
+				push @problems, $session->html_phrase( "validate:invalid_orcid" );
+			}
+		}
+		else
+		{
+			push @problems, $session->html_phrase( "validate:invalid_orcid" );
+		}		
+	}
 
         $self->{repository}->run_trigger( EPrints::Const::EP_TRIGGER_VALIDATE_FIELD(),
                 field => $self,
